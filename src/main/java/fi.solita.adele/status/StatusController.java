8460aeaef9ab4966d074aac40589cad2e7873f2c
Liisa Sailaranta <liisa.sailaranta@solita.fi>
2016-06-04 14:53:00 +0300

    Status page counts events in the event table.

--- src/main/java/fi.solita.adele/status/StatusController.java
+++ src/main/java/fi.solita.adele/status/StatusController.java
@@ -1,16 +1,30 @@
 package fi.solita.adele.status;
 
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.transaction.annotation.Transactional;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
+import javax.annotation.Resource;
+
 @CrossOrigin
 @RestController
 public class StatusController {
 
+    @Resource
+    private JdbcTemplate jdbcTemplate;
+
+    private static final String SCHEMA_VERSION = "event";
+
     @RequestMapping(value = "/status", method = RequestMethod.GET)
-    String hello() {
-        return "ok";
+    String applicationStatus() {
+        return "ok, " + eventsCount() + " events";
+    }
+
+    @Transactional
+    private Integer eventsCount()  {
+        return jdbcTemplate.queryForObject("select count(*) from " + SCHEMA_VERSION, Integer.class);
     }
 }
